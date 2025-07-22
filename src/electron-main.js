9112160a3e9e25460f99bea0725c2ee51f5917ee
James Baldwin <james.baldwin3232@gmail.com>
2025-07-22 18:39:36 +0100

    login

---
+++ src/electron-main.js
@@ -0,0 +1,21 @@
+const { app, BrowserWindow } = require('electron');
+const path = require('path');
+
+process.env.ELECTRON_DISABLE_SECURITY_WARNINGS = 'true';
+
+function createWindow () {
+  const win = new BrowserWindow({
+    width: 1920,
+    height: 1080,
+    webPreferences: {
+      nodeIntegration: true,
+      contextIsolation: false
+    }
+  });
+
+  // absolute path → file://… URL
+  const startPage = path.resolve(__dirname, 'index.html');
+  win.loadURL('file://' + startPage);
+}
+
+app.whenReady().then(createWindow);
